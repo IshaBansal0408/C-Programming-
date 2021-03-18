76176da0a82e7aabc42cac22f552f40634d7e73e
Isha Bansal <isha.bansal1504@gmail.com>
2021-03-18 14:32:38 +0530

    random

--- 002. Pattern Problems/004. Pattern 4.cpp
+++ 002. Pattern Problems/004. Pattern 4.cpp
@@ -1,26 +1,56 @@
 #include<iostream>
 using namespace std;
 
+void space1(int cur, int mid,int n1){
+    int val;
+    if(cur==mid){
+        val=0;
+    }
+    else{
+        val=(abs(mid-cur));
+    }
+    for(int i=0;i<val;i++){
+        cout<<" ";
+    }
+}
+void stars(int cur,int mid,int n1){
+    int val;
+    if(cur<mid){
+        val=2*cur+1;
+    }
+    else if(cur==mid){
+        val=n1;
+    }
+    else{
+        val=n1-cur;
+    }
+    for(int i=0;i<val;i++){
+        cout<<"*";
+    }
+}
+void space2(int cur, int mid,int n1){
+    int val;
+    if(cur==mid){
+        val=0;
+    }
+    else{
+        val=(abs(mid-cur));
+    }
+    for(int i=0;i<val;i++){
+        cout<<" ";
+    }
+}
 void pattern(int n){
     int mid=n/2;
-    for(int i=1;i<=n;i++){
-        if(i<mid){
-            int stars=2*i - 1;
-            for (int s=0;s<stars;s++)
-                cout<<"*"<<" ";
+    for(int i=0;i<n;i++){
+            int x=i;
+            if(i>mid){
+                x=n-(x+1);
+            }
+            space1(x,mid,n);
+            stars(x,mid,n);
+            space2(x,mid,n);
             cout<<endl;
-        }
-        else if(i==mid){
-            for (int s=0;s<n;s++)
-                cout<<"*"<<" ";
-            cout<<endl;
-        }
-        else{
-            int stars = n-i+1;
-            for (int s=0;s<stars;s++)
-                cout<<"*"<<" ";
-            cout<<endl;
-        }
     }
 }
 int main(){
