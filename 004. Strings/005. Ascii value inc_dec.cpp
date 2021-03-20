1fbc48d75cfbf12419b8487211fb3ae06927ae62
Isha Bansal <isha.bansal1504@gmail.com>
2021-03-20 20:50:12 +0530

    string codes 2

--- 004. Strings/005. Ascii value inc_dec.cpp
+++ 004. Strings/005. Ascii value inc_dec.cpp
@@ -1,16 +1,27 @@
 #include<iostream>
 using namespace std;
 
-string cal(string str){
-    int l=str.length();
+string cal(string str1){
+    int l=str1.length(),z;
+    char t;
     for(int i=0;i<l;i++){
-        char c=str[i];
-        str[i]=char(int(str[i]));
+            char c=str1[i];
+            if((i+1)%2==0){
+                z=int(str1[i])-1;
+                t=char(z);
+                str1[i]=t;
+            }
+            else{
+                z=int(str1[i])+1;
+                t=char(z);
+                str1[i]=t;
+            }
     }
-    return str;
+    return str1;
 }
 int main(){
-    string s;cin>>s;
+    string s;
+    cin>>s;
     string t=cal(s);
     cout<<t;
 }
