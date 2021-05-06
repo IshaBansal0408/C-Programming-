4eb37dfd46ff290015b3653e9467a5ee22e6e327
IshaBansal0408 <isha.bansal1504@gmail.com>
2021-05-06 21:57:28 +0530

    DLL inserstion complete

--- 02. May - 2021/001. Linked Lists/02. Doubly Linked List/Doubly_LL.cpp
+++ 02. May - 2021/001. Linked Lists/02. Doubly Linked List/Doubly_LL.cpp
@@ -40,7 +40,7 @@ public:
             header = new_node;
             new_node->next = temp;
             new_node -> prv = NULL;
-            new_node->next->prv = temp;
+            temp->prv = new_node;
         }
         size_of_DLL+=1;
     }
@@ -60,10 +60,39 @@ public:
             temp -> next = new_node;
             new_node -> prv = temp;
             new_node -> next = NULL;
+            tail = new_node;
         }
         size_of_DLL+=1;
     }
 
+    // Insert at kth position
+    void insert_at_k(int data,int k){
+        if(k<1 || k>size_of_DLL+1){
+            return;
+        }
+        else if(k==1){
+            insert_start(data);
+        }
+        else if(k==size_of_DLL+1){
+            insert_end(data);
+        }
+        else{
+            int i = 1;
+            Node* new_node = new Node(data);
+            Node* temp = header;
+            while(i+1!=k){
+                temp = temp -> next;
+                i+=1;
+            }
+            temp->next->prv = new_node;
+            new_node -> next = temp -> next;
+            temp -> next = new_node;
+            new_node -> prv = temp;
+            size_of_DLL+=1;
+        }
+    }
+
+    // Get size of the list
     int get_size(){
         return size_of_DLL;
     }
@@ -74,6 +103,16 @@ public:
         while(temp!=NULL){
             cout<<temp->data<<" ";
             temp= temp->next;
+        }
+        cout<<endl;
+    }
+
+    // Print in reverse
+    void print_DLL_rev(){
+        Node* temp = tail;
+        while(temp!=NULL){
+            cout<<temp->data<<" ";
+            temp = temp -> prv;
         }
         cout<<endl;
     }
@@ -88,10 +127,35 @@ int main(){
     cout<<"The size of list is: "<<DLL.get_size()<<endl;
     cout<<"Printing the list..."<<endl;
     DLL.print_DLL();
+    cout<<"Printing the list in reverse..."<<endl;
+    DLL.print_DLL_rev();
     cout<<"Inserting in the end..."<<endl;
     DLL.insert_end(4);
     DLL.insert_end(5);
     DLL.insert_end(6);
+    cout<<"The size of list is: "<<DLL.get_size()<<endl;
+    cout<<"Printing the list..."<<endl;
+    DLL.print_DLL();
+    cout<<"Printing the list in reverse..."<<endl;
+    DLL.print_DLL_rev();
+
+    cout<<"Inserting at nth place...."<<endl;
+    int n;
+
+    cin>>n;
+    DLL.insert_at_k(200,n);
+    cout<<"The size of list is: "<<DLL.get_size()<<endl;
+    cout<<"Printing the list..."<<endl;
+    DLL.print_DLL();
+
+    cin>>n;
+    DLL.insert_at_k(20,n);
+    cout<<"The size of list is: "<<DLL.get_size()<<endl;
+    cout<<"Printing the list..."<<endl;
+    DLL.print_DLL();
+
+    cin>>n;
+    DLL.insert_at_k(0,n);
     cout<<"The size of list is: "<<DLL.get_size()<<endl;
     cout<<"Printing the list..."<<endl;
     DLL.print_DLL();
